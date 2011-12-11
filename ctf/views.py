7fecd18e06d9fb25fd6da1874b594e3d6f3d9acf
Nick Charlton <hello@nickcharlton.net>
2011-12-11 10:53:20 +0000

    Adds a template for the player.
    * presents it.

--- ctf/views.py
+++ ctf/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render_to_response
 
 def player(request, player_name):
-    return HttpResponse('/player/%s' % player_name)
+    return render_to_response('player.html', {'player_name': player_name})
 
 def new_player(request):
     return HttpResponse('/player/new')
